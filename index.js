10d76efc49357b84483654278501353dd23e615a
+ <+>
2022-07-22 23:15:35 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your code here!
+// // // // Write your code here!
+// const element = document.createElement("div");
+// document.body.append(element);
+
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement("li");
+//     li.textContent = (i + 1).toString();
+//     ul.append(li);
+//   }
+  
+//   element.append(ul);
+
+// const main = document.getElementById("main");
+// main.innerHTML =
+//   "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+
+// element.style.height = "300px";
+// element.style.backgroundColor = "#27647B";
+
+// element.textContent = "You've changed what's on the screen!";
+
+// element.style.fontSize = "24px";
+// element.style.marginLeft = "30px";
+// element.style.lineHeight = 2;
+
+// element.className = "pet-listing dog";
+
+
+
+// element.classList.remove("dog");
+// element.classList.add("cat", "sale");
+
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory"
+
+newHeader.innerHTML = "ST is the champion!"
+  
+
+  
